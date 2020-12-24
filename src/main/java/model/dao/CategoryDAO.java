4273c56e69e205df0322aeae5303fb98f135db80
FraMatt97 <73857402+Fra-Hub97@users.noreply.github.com>
2020-12-24 20:20:58 +0100

    CategoryDAOTest done and Category Checkstyle done.

--- src/main/java/model/dao/CategoryDAO.java
+++ src/main/java/model/dao/CategoryDAO.java
@@ -1,44 +1,40 @@
 package model.dao;
 
-import model.bean.Category;
-
 import java.sql.*;
 import java.util.ArrayList;
+import model.bean.Category;
 
 public class CategoryDAO {
 
     public Category doRetrieveByName(String name) {
-        Connection cn = null;
         try {
-            cn = ConPool.getConnection();
+            Connection cn = ConPool.getConnection();
             Statement st = cn.createStatement();
             Category category = null;
             ResultSet rs = st.executeQuery("SELECT * FROM Category C WHERE C.name='" + name + "';");
-            if(rs.next()){
+            if (rs.next()) {
                 category = new Category(rs.getString(1), rs.getString(2), rs.getString(3));
             }
             st.close();
             cn.close();
             return category;
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             return null;
         }
     }
 
     public void doDeleteByName(String name) {
-        Connection cn = null;
         try {
-            cn = ConPool.getConnection();
+            Connection cn = ConPool.getConnection();
             Statement st = cn.createStatement();
             st.executeUpdate("DELETE FROM Category WHERE name='" + name + "';");
             st.close();
             cn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
     public void doSave(Category c) {
         try {
             Connection cn = ConPool.getConnection();
@@ -52,13 +48,13 @@ public class CategoryDAO {
             }
             ps.close();
             cn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public void doUpdateByName(Category c, String oldName){
-        try{
+
+    public void doUpdateByName(Category c, String oldName) {
+        try {
             Connection cn = ConPool.getConnection();
             PreparedStatement ps = cn.prepareStatement(
                     "update Category set name=?, description=?, image=? where name=?");
@@ -71,17 +67,17 @@ public class CategoryDAO {
             }
             ps.close();
             cn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public ArrayList<Category> doRetrieveAll() throws SQLException{
+
+    public ArrayList<Category> doRetrieveAll() throws SQLException {
         Connection cn = ConPool.getConnection();
         Statement st = cn.createStatement();
         ArrayList<Category> categories = new ArrayList<>();
         ResultSet rs = st.executeQuery("SELECT * FROM Category WHERE 1=1;");
-        while(rs.next()){
+        while (rs.next()) {
             Category category = new Category(rs.getString(1), rs.getString(2),
                     rs.getString(3));
             categories.add(category);
---
+++ src/test/java/model/dao/CategoryDAOTest.java
@@ -0,0 +1,137 @@
+package model.dao;
+
+import model.bean.Category;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryDAOTest {
+    private final CategoryDAO cd=new CategoryDAO();
+
+    @Test
+    void doRetrieveByNameOk() {
+        Category cat1= new Category("Sparatutto","sparatutto per tutti","path");
+        cd.doSave(cat1);
+        Category cat2=cd.doRetrieveByName("Sparatutto");
+        assertTrue(cat1.getName().equals(cat2.getName()) && cat1.getDescription().equals(cat2.getDescription())
+                            && cat1.getImage().equals(cat2.getImage()));
+        cd.doDeleteByName(cat1.getName());
+    }
+
+    @Test
+    void doRetrieveByNameNotOkNull() {
+        assertNull(cd.doRetrieveByName("'limit"));
+
+    }
+
+    @Test
+    void doRetrieveByNameNotOk() {
+        assertNull(cd.doRetrieveByName("/'1"));
+
+    }
+
+    @Test
+    void doDeleteByNameOk() {
+        Category cat1 = new Category("CategoriaTest","descrizione per il testing","path");
+        cd.doSave(cat1);
+        cd.doDeleteByName(cat1.getName());
+        assertNull(cd.doRetrieveByName(cat1.getName()));
+    }
+
+    @Test
+    void doDeleteByNameNotOkNull() {
+        assertThrows(RuntimeException.class,()->cd.doDeleteByName("'limit"));
+    }
+
+    @Test
+    void doDeleteByNameNotOk() {
+        assertThrows(RuntimeException.class,()->cd.doDeleteByName("/'1"));
+    }
+
+    @Test
+    void doSaveOk() {
+        Category cat1=new Category("Avventura","Avventura per tutti","path");
+        cd.doSave(cat1);
+        Category cat2=cd.doRetrieveByName("Avventura");
+        assertTrue(cat1.getName().equals(cat2.getName()) && cat1.getDescription().equals(cat2.getDescription())
+                            && cat1.getImage().equals(cat2.getImage()));
+        cd.doDeleteByName(cat1.getName());
+    }
+
+    @Test
+    void doSaveNotOk() {
+        Category cat1= new Category("TestxSaveNotOk","test di prova","path");
+        cd.doSave(cat1);
+        assertThrows(RuntimeException.class,()->cd.doSave(cat1));
+        cd.doDeleteByName(cat1.getName());
+    }
+
+    @Test
+    void doUpdateByNameOk() {
+        Category cat1 = new Category("TestxUpdateOk1","test di prova1","path1");
+        cd.doSave(cat1);
+        String oldName = cat1.getName();
+        String oldDesc = cat1.getDescription();
+        String oldImage = cat1.getImage();
+        cat1.setName("TestxUpdateOk2");
+        cat1.setDescription("test di prova2");
+        cat1.setImage("path2");
+        cd.doUpdateByName(cat1, oldName);
+        cat1=cd.doRetrieveByName(cat1.getName());
+        assertFalse(cat1.getName().equals(oldName) && cat1.getDescription().equals(oldDesc) &&
+                            cat1.getImage().equals(oldImage));
+        cd.doDeleteByName(cat1.getName());
+    }
+
+    @Test
+    void doUpdateByNameNotOk() {
+        Category cat1 = new Category("TestxUpdateNotOk","test di prova1","path1");
+        assertThrows(RuntimeException.class,()->cd.doUpdateByName(cat1,"/'1"));
+    }
+
+    @Test
+    void doUpdateByNameNotOkNull() {
+        Category cat1 = new Category("TestxUpdateNotOkNull","test di prova1","path1");
+        assertThrows(RuntimeException.class,()->cd.doUpdateByName(cat1,"'limit"));
+
+    }
+
+    @Test
+    void doRetrieveAllxNone() {
+        try {
+            ArrayList<Category> list=cd.doRetrieveAll();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Test
+    void doRetrieveAllxOne() {
+        Category cat1=new Category("TestxRetrieveAllxOne","test di prova1","path1");
+        cd.doSave(cat1);
+        try {
+            ArrayList<Category> list=cd.doRetrieveAll();
+            cd.doDeleteByName(cat1.getName());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Test
+    void doRetrieveAllxAll() {
+        Category cat1=new Category("TestxRetrieveAllxOne1","test di prova1","path1");
+        Category cat2=new Category("TestxRetrieveAllxOne2","test di prova1","path1");
+        cd.doSave(cat1);
+        cd.doSave(cat2);
+        try {
+            ArrayList<Category> list=cd.doRetrieveAll();
+            cd.doDeleteByName(cat1.getName());
+            cd.doDeleteByName(cat2.getName());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
