2734a6eb25a6eed2dbc43a06d3d5905f1dc53b51
GerardoBrescia <g.brescia3@studenti.unisa.it>
2021-01-01 11:25:17 +0100

    Modifiche ad alcuni file già esistenti

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -11,10 +11,15 @@ import model.bean.Category;
 import model.bean.DigitalProduct;
 import model.bean.PhysicalProduct;
 import model.bean.Product;
+import model.bean.Tag;
 import model.dao.CategoryDAO;
 import model.dao.DigitalProductDAO;
 import model.dao.PhysicalProductDAO;
+import model.dao.TagDAO;
 
+/**
+ * This class allows to inizialize the Homepage.
+ */
 @WebServlet("/index.html")
 public class HomeServlet extends HttpServlet {
 
@@ -25,11 +30,21 @@ public class HomeServlet extends HttpServlet {
         PhysicalProductDAO fpDao = new PhysicalProductDAO();
         ArrayList<DigitalProduct> prodottiDig;
         ArrayList<PhysicalProduct> prodottiFis;
+        ArrayList<DigitalProduct> prodottiSecond;
         prodottiDig = dpDao.doRetrieveAllByTag("New", 0, 2);
         prodottiFis = fpDao.doRetrieveAllByTag("New", 0, 2);
         ArrayList<Product> nuoviProdotti = new ArrayList<>();
         nuoviProdotti.addAll(prodottiDig);
         nuoviProdotti.addAll(prodottiFis);
+
+        TagDAO tagDAO = new TagDAO();
+        ArrayList<Tag> tags;
+        tags = tagDAO.doRetrieveByNameFragment("",0,100);
+        Random rand = new Random();
+        int n = rand.nextInt(tags.size());
+        Tag tag = tags.get(n);
+        System.out.println(tag.getName());
+        prodottiSecond = dpDao.doRetrieveByAllFragment("","",999999.0,  "", tag.getName(),"", 0,4);
 
         ArrayList<Category> categorie = null;
         try {
@@ -37,6 +52,8 @@ public class HomeServlet extends HttpServlet {
         } catch (SQLException throwables) {
             //lanciare eccezioni
         }
+
+        getServletContext().setAttribute("secondProducts", prodottiSecond);
         getServletContext().setAttribute("newProducts", nuoviProdotti);
         getServletContext().setAttribute("categorie", categorie);
         super.init();
