0302006bf9ac326c198df3e307e56a7556969ecf
Attornado <73835871+Attornado@users.noreply.github.com>
2021-01-05 18:15:47 +0100

    Aggiunta controlli su CheckMailServlet e CheckUsernameServlet, per controlli ajax su username e mail; Correzione a searchBarAsync.js;

---
+++ src/main/java/controller/CheckUsernameServlet.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import model.dao.UserDAO;
+
+@WebServlet("/check-username")
+public class CheckUsernameServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        String username = req.getParameter("username");
+        UserDAO ud = new UserDAO();
+        if (username.matches(LoginServlet.USERNAME_REGEX)
+                && ud.doRetrieveByUsername(username) == null) {
+            resp.getWriter().println("<ok/>");
+        } else {
+            resp.getWriter().println("<notOk/>");
+        }
+        resp.flushBuffer();
+    }
+}
