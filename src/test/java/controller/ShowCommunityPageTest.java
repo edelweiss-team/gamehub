42f6eee0758b9ca7ff6d556e17eebfd7a87b54c2
FraMatt97 <73857402+Fra-Hub97@users.noreply.github.com>
2021-01-13 18:43:54 +0100

    Testing della showCommunityServlet e GetMoreCategoriesServlet.

---
+++ src/test/java/controller/ShowCommunityPageTest.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShowCommunityPageTest {
+    private ShowCommunityPage servlet;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    @BeforeEach
+    void setUp() {
+        servlet = new ShowCommunityPage();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        BasicConfigurator.configure();
+    }
+
+    @Test
+    public void showCommunityOk() throws ServletException, IOException {
+        servlet.doPost(request, response);
+        assertEquals("/WEB-INF/view/Community.jsp", response.getForwardedUrl());
+    }
+}
