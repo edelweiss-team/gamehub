1a392e9a924da2e24d0fd6d4d9183f5e5ce12cb4
FraMatt97 <73857402+Fra-Hub97@users.noreply.github.com>
2021-01-13 21:12:50 +0100

    Testing della GetMoreTagsServletTest e aggiornamento GetMoreCategoriesServletTest.

--- src/test/java/controller/GetMoreTagsServletTest.java
+++ src/test/java/controller/GetMoreTagsServletTest.java
@@ -11,62 +11,57 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GetMoreCategoriesServletTest {
-    private GetMoreCategoriesServlet servlet;
+class GetMoreTagsServletTest {
+    private GetMoreTagsServlet servlet;
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
 
     @BeforeEach
     void setUp() {
-        servlet = new GetMoreCategoriesServlet();
+        servlet = new GetMoreTagsServlet();
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
         BasicConfigurator.configure();
     }
 
     @Test
-    public void searchStringNotOk() throws ServletException, IOException {
-        request.addParameter("search","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        assertThrows(RequestParametersException.class, ()->servlet.doPost(request, response));
-    }
-
-    @Test
-    public void getMoreCategoriesWithoutSearch() throws ServletException, IOException{
-        request.addParameter("categoriesPerRequest","8");
+    public void getMoreTagsWithSearch() throws ServletException, IOException {
+        request.addParameter("search","New");
+        request.addParameter("tagsPerRequest","8");
         request.addParameter("startingIndex","1");
         servlet.doPost(request, response);
         assertTrue( !response.getContentAsString().isEmpty());
     }
 
     @Test
-    public void getMoreCategoriesSearching() throws ServletException, IOException{
-        request.addParameter("search","Azione");
-        request.addParameter("categoriesPerRequest","8");
+    public void getMoreTagsWithoutSearch() throws ServletException, IOException {
+        request.addParameter("search","");
+        request.addParameter("tagsPerRequest","8");
         request.addParameter("startingIndex","1");
         servlet.doPost(request, response);
         assertTrue( !response.getContentAsString().isEmpty());
     }
 
     @Test
-    public void getMoreCategoriesWithoutLength() throws ServletException, IOException{
-        request.addParameter("search","Azione");
+    public void getMoreTagsWithoutLength() throws ServletException, IOException{
+        request.addParameter("search","New");
         request.addParameter("startingIndex","1");
         servlet.doPost(request, response);
         assertTrue( !response.getContentAsString().isEmpty());
     }
 
     @Test
-    public void getMoreCategoriesIndexNotOk() throws ServletException, IOException{
-        request.addParameter("search","Azione");
-        request.addParameter("categoriesPerRequest","8");
+    public void getMoreTagsIndexNotOk() throws ServletException, IOException{
+        request.addParameter("search","New");
+        request.addParameter("tagsPerRequest","8");
         request.addParameter("startingIndex","12&/a");
         assertThrows(RequestParametersException.class, ()->servlet.doPost(request, response));
     }
 
     @Test
-    public void getMoreCategoriesNone() throws ServletException, IOException{
+    public void getMoreTagsNone() throws ServletException, IOException{
         request.addParameter("search","Azione");
-        request.addParameter("categoriesPerRequest","0");
+        request.addParameter("tagsPerRequest","0");
         request.addParameter("startingIndex","1");
         servlet.doPost(request, response);
         assertTrue( !response.getContentAsString().isEmpty());
@@ -75,7 +70,7 @@ class GetMoreCategoriesServletTest {
     @Test
     public void getMoreCategoriesOne() throws ServletException, IOException{
         request.addParameter("search","Azione");
-        request.addParameter("categoriesPerRequest","1");
+        request.addParameter("tagsPerRequest","1");
         request.addParameter("startingIndex","1");
         servlet.doPost(request, response);
         assertTrue( !response.getContentAsString().isEmpty());
